d96d89a9a1ba0269c32fd07002315fba397ae94b
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-01-13 16:44:11 -0600

    corrected issue with new logo location on outro component

--- src/components/MainPage/Outro/index.js
+++ src/components/MainPage/Outro/index.js
@@ -1,10 +1,11 @@
+import Logo from "../../../logo.svg";
 
 function Outro(){
   return(
     <div className="text-center highlight">
       Like what you see?  Click my logo to check out my resume page for contact information
       <a target="_blank" href="https://brian-fairbanks.github.io/Resume/">
-        <img className="logo" src={require("../../../logo.svg")} alt="BF Logo"/>
+        <img className="logo" src={Logo} alt="BF Logo"/>
       </a>
     </div>
   )
